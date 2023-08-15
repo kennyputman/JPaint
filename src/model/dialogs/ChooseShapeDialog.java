4eeaeb65fe95297473013d2adbfbd2b8f923ee74
Kenny Putman <kbputman@gmail.com>
2023-08-15 15:42:01 -0500

    update ChooseShapeDialog to ignore Group type

--- src/model/dialogs/ChooseShapeDialog.java
+++ src/model/dialogs/ChooseShapeDialog.java
@@ -4,6 +4,10 @@ import model.types.ShapeType;
 import model.interfaces.IApplicationState;
 import view.interfaces.IDialogChoice;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ChooseShapeDialog implements IDialogChoice<ShapeType> {
     private final IApplicationState applicationState;
 
@@ -24,7 +28,9 @@ public class ChooseShapeDialog implements IDialogChoice<ShapeType> {
 
     @Override
     public ShapeType[] getDialogOptions() {
-        return ShapeType.values();
+        return Stream.of(ShapeType.values())
+                .filter(type -> type != ShapeType.GROUP)
+                .toArray(ShapeType[]::new);
     }
 
     @Override
